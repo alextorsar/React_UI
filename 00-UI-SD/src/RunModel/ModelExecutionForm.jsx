332e828045be817b4f88307d8db1b05ec9b1f3fa
alextorsar <alejandro.torres.sar@gmail.com>
2024-01-31 19:02:30 +0100

    Adding basic stepper to ModelExecutionForm

--- 00-UI-SD/src/RunModel/ModelExecutionForm.jsx
+++ 00-UI-SD/src/RunModel/ModelExecutionForm.jsx
@@ -6,7 +6,18 @@ import {
     ModalCloseButton,
     ModalContent,
     ModalOverlay,
-    ModalHeader
+    ModalHeader,
+    Step,
+    StepDescription,
+    StepIcon,
+    StepIndicator,
+    StepNumber,
+    StepSeparator,
+    StepStatus,
+    StepTitle,
+    Stepper,
+    useSteps,
+    Box
 } from '@chakra-ui/react';
 
 const theme = extendTheme({
@@ -25,7 +36,17 @@ const theme = extendTheme({
     },
 });
 
+const steps = [
+    { title: 'Initial Conditions'},
+    { title: 'Params'},
+    { title: 'Run'}
+]
+
 export function ModelExecutionForm({ isOpen, onClose }) {
+    const { activeStep, setActiveStep } = useSteps({
+        index: 1,
+        count: 3,
+    })
     return(
         <ChakraProvider theme={theme}>
             <Modal size="xl" isOpen={isOpen} onClose={onClose} isCentered>
@@ -33,8 +54,25 @@ export function ModelExecutionForm({ isOpen, onClose }) {
                 <ModalContent width="100%" height="80%" minHeight="80%" maxHeight="80%"  display="flex" alignItems="center">
                     <ModalHeader height="10%">Initial Conditions</ModalHeader>
                     <ModalCloseButton />
-                    <ModalBody width="100%" height="90%" minHeight="90%"  maxHeight="90%" display="flex" alignItems="center">
-
+                    <ModalBody width="100%" height="90%" minHeight="90%"  maxHeight="90%" display="flex" alignItems="center" justifyContent="center">
+                        <Stepper index={activeStep} width="80%">
+                            {steps.map((step, index) => (
+                                <Step key={index}>
+                                <StepIndicator>
+                                    <StepStatus
+                                    complete={<StepIcon />}
+                                    incomplete={<StepNumber />}
+                                    active={<StepNumber />}
+                                    />
+                                </StepIndicator>
+                                <Box flexShrink='0'>
+                                    <StepTitle>{step.title}</StepTitle>
+                                    <StepDescription>{step.description}</StepDescription>
+                                </Box>
+                                <StepSeparator />
+                                </Step>
+                            ))}
+                        </Stepper>
                     </ModalBody>
                 </ModalContent>
             </Modal>
